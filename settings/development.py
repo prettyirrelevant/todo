3e026906c5ee8d82217f651b9bbef520744e0b00
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-25 09:46:42 +0100

    dev settings and .flaskenv

---
+++ settings/development.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from os import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = '%f6zsA<@hcb|5JbeNGnI!/NEX}^dzGS)%?AVKBNxJ]z9&8"ta)}#ZS-dRlBexBc'
+
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR}/todo.db"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+CLIENT_ID = environ.get("CLIENT_ID")
+
+CLIENT_SECRET = environ.get("CLIENT_SECRET")
+
+SOCIAL_AUTH_AUTH0_DOMAIN = "prettyirrelevant.us.auth0.com"

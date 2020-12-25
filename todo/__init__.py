966b6b1ba981c519667f63686de6ac5d0acacacd
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-25 09:51:33 +0100

    factory settings

---
+++ todo/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+
+
+def create_app(dev_settings):
+    app = Flask(__name__)
+    app.config.from_object(dev_settings)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from . import routes
+
+        return app

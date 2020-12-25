62d7d530795247f30b0843452eda3e6a8542ed99
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-25 10:55:28 +0100

    update __init__.py

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_client import OAuth
 
 db = SQLAlchemy()
 migrate = Migrate()
+oauth = OAuth()
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
@@ -17,6 +19,7 @@ def create_app(dev_settings):
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+    oauth.init_app(app)
 
     with app.app_context():
         from . import routes

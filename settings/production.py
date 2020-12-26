0f52662fb357327b37ccfcabf5b79ece84573a30
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-26 20:12:23 +0100

    oh my :|

--- settings/production.py
+++ settings/production.py
@@ -5,3 +5,5 @@ SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
 CALLBACK_URL = environ.get("CALLBACK_URL")
 
 SECRET_KEY = environ.get("SECRET_KEY")
+
+AUTH0_DOMAIN = environ.get("AUTH0_DOMAIN")

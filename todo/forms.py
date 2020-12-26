5113437df9c394a00520461a4ae9f6f5fb793a56
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-26 14:57:55 +0100

    changed to textarea

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import Length, DataRequired
 
 
 class AddTodoForm(FlaskForm):
     tags = StringField("Tags", validators=[Length(max=150)])
-    text = HiddenField("Text", validators=[DataRequired(), Length(min=1)])
+    text = TextAreaField("Text", validators=[DataRequired(), Length(min=1)])

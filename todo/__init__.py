7c41309a527285af3e9b1f744acd67ce0cd1bbf1
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-26 14:35:29 +0100

    added flask-moment

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,11 +1,13 @@
+from authlib.integrations.flask_client import OAuth
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from authlib.integrations.flask_client import OAuth
 
 db = SQLAlchemy()
 migrate = Migrate()
+moment = Moment()
 oauth = OAuth()
 
 login_manager = LoginManager()
@@ -18,6 +20,7 @@ def create_app(dev_settings):
 
     db.init_app(app)
     login_manager.init_app(app)
+    moment.init_app(app)
     migrate.init_app(app, db)
     oauth.init_app(app)
 
